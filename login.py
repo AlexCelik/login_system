ebfacb2b2a64c536c7c2a7baf52e59f4dabba32f
Venom23 <venomafamilia@gmail.com>
2018-10-13 17:49:18 +0100

    Login system with an encryption with the base64 module

--- login.py
+++ login.py
@@ -1,33 +1,55 @@
+import base64
+
 
 def newuser():
     Username = input("Choose an Username : ")
     Password = input("Choose a Password : ")
     Passwordchecker = input("Confirm the Password : ")
-    if Password != Passwordchecker:
+    password_try = 3
+    while Password != Passwordchecker:
         print("Error, the password does not match")
-        newuser()
-    f = open("Userdata.txt",'w')
-    f.write(Username )
-    f.write("\n"+Password)
+        Password = input("Choose a Password : ")
+        Passwordchecker = input("Confirm the Password : ")
+        password_try -= 1
+        print("You have ", password_try, " attempt left.")
+        if password_try <= 0:
+            print("you have tried too much wrong password, please try again later")
+            break
+    f = open("Userdata.txt", 'a')
+
+    ########### Encoding Password ##############
+    encoded = base64.b64encode(Password.encode("utf-8")).decode("utf-8")
+
+    f.write(Username)
+    f.write(",")
+    f.write(encoded)
+    f.write("\n")
+
     f.close()
 
 
 def login():
-    lines = []
-    with open ('Userdata.txt', 'rt') as in_file:
-        for line in in_file:
-            lines.append(line.rstrip('\n'))
+    takingFromFile = open("Userdata.txt", "r")
+
     Username = input("Username : ")
     Password = input("Password : ")
-    if Username != lines[0]:
-        print("wrong Username")
-    elif Password != lines[1]:
-        print("Password doesnt match")
-    elif Username == lines[0] and Password == lines[1]:
-        print("welcome to the program")
+
+    for lines in takingFromFile:
+        lines = lines.strip()
+        uName, pWord = lines.split(",")
+
+        decpWord = base64.b64decode(pWord).decode("utf-8")
+
+        if Username == uName:
+            if Password == decpWord:
+                print("welcome to the program")
+                exit()
 
 
-
-
-newuser()
-login()
+UserInput = input("Login = y  , Create a new User = n  : ")
+if UserInput == "n":
+    newuser()
+elif UserInput == "y":
+    login()
+else:
+    print("Wrong Character, please type 'y' for login or 'n' for create a new user. ")
